26ce3ac6351a469b04f58e88776048a484c65be9
farhatbaig <farhatbaig77@gmail.com>
2025-09-01 16:17:22 +0500

    add test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,20 +11,18 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:my_app/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    testWidgets('Reward screen UI smoke test', (WidgetTester tester) async {
+      await tester.pumpWidget(const MyApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+      // let async/timers complete
+      await tester.pumpAndSettle(const Duration(seconds: 1));
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+      expect(find.text("You've unlocked a \$10 reward!"), findsOneWidget);
+      expect(find.text('Choose Brand'), findsOneWidget);
+      expect(find.text('\$420.99'), findsOneWidget);
+      expect(find.text('Paid'), findsOneWidget);
+    });
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
-  });
+
+
 }
